9e18421e59449f953b12038666f3901956f3d902
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2022-05-17 06:21:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -76,14 +76,164 @@ class RoleBasedDB
 
 class NavBarMenu
 {
-  public $menu = array();
+    
+   
 
-  public function get_root_menu($sql)
-  {
-    $db = new RoleBasedDB;
-    $res = $db->query($sql);
-    $this->menu=$res;
-  }
+    public function get_menu($get_id="", $gt_id_parent="", $get_type="", $get_HasChild="", $get_link="")
+    {
+      ////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////
+        ////////////////////                    ////////////////
+        /////////////////   function description  //////////////
+        /////////////////                         //////////////
+        // this function genrate a dynamic query based on the //
+        //parameters which you put and return an array with   //
+        // the query itself, query result, row count, also    //
+        // the returned data into a multi dimention array     //
+        // result
+        // 1- sql_query the dynamic genrated sql quer itself 
+        // 2- queryResult  the reslut in case there is sql error
+        // 3- resCount the row count
+        // 4- navbar the row data itself
+        ////////////////////////////////////////////////////////
+        // Developed by engineer Muhammad Abdulla El Nahtta   //
+        // Cell Phone +20 1093001070                          //
+        ////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////// 
+
+        // new object for Arabicss db 
+        $db = new RoleBasedDB;
+        //php Creating a dynamic search query with PHP and MySQL
+        $whereArr = array();
+        if ($get_id != "")
+        {
+        $whereArr[] = "`id` = '{$get_id}'";
+        }
+        if ($gt_id_parent != "")
+        {
+        $whereArr[] = "`id_parent` = '{$gt_id_parent}'";
+        }
+        if ($get_type != "")
+        {
+        $whereArr[] = "`type` = '{$get_type}'";
+        }
+        if ($get_HasChild != "") {
+          $whereArr[] = "`HasChild` = '{$get_HasChild}'";
+        } 
+
+        if ($get_link != "") {
+          $whereArr[] = "`link` IS NOT NULL";
+        } 
+
+
+        // escaping the WHERE clause in case no parameter entered 
+        // Array to string for WHERE clause  
+        if ($whereArr == null) {
+          $whereStr = "";
+        } else {
+          $whereStr = "WHERE " . implode(" AND ", $whereArr);
+        }
+
+        if ($get_HasChild != "") {
+          // getting how many child per id if the value passed
+          $menu_sql_query ="SELECT * FROM (SELECT m1.*, 
+          (SELECT COUNT(*) FROM `module_menue` `m2` WHERE `m2`.`id_Parent`=`m1`.`id`)
+          AS `HasChild` FROM `module_menue` `m1`) AS `testy` {$whereStr} ORDER BY `order_no`;";
+
+        } else {
+          // getting only the main 
+          $menu_sql_query ="SELECT * FROM `module_menue` {$whereStr} ORDER BY `order_no`;";
+
+        }
+
+        // genrate mysql query
+        $queryResult = $db->query($menu_sql_query);
+
+        while($row = $queryResult->fetch_assoc()) {
+          $id[] = $row["id"];
+          $id_parent[] = $row["id_parent"];
+          $icon[] = $row["icon"];
+          $link[] = $row["link"];
+          $name[] = $row["name"];
+          $type[] = $row["type"];
+          $order_no [] = $row["order_no"];
+        }
+        
+        $navBar= array("id"=>$id, "id_parent"=>$id_parent, "icon"=>$icon,
+          "link"=>$link, "name"=>$name, "type"=>$type, "order_no"=>$order_no);
+          
+          // rest variables to avoid confilct 
+          $id[] = '';
+          $id_parent 	[] = '';
+          $icon[] = '';
+          $link[] = '';
+          $name[] = '';
+          $type[] = '';
+          $order_no [] ='';
+
+
+        $resCount = $queryResult->num_rows;
+
+        $query_data = array("sql_query"=>$menu_sql_query, "queryResult"=>$queryResult, "resCount"=>$resCount, "navbar"=>$navBar);
+        return $query_data;
+
+
+    }
+
+    public function html_menu($tabCount, $tabArray)
+    {
+      ////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////
+      ////////////////////                    ////////////////
+      /////////////////   function description  //////////////
+      /////////////////                         //////////////
+      // this function genrate html based on the parameters //
+      // which you put the count of tabs and the array which//
+      // has the data itself and return a string which      //
+      // contain the html <li> menu                         //
+      ////////////////////////////////////////////////////////
+      // Developed by engineer Muhammad Abdulla El Nahtta   //
+      // Cell Phone +20 1093001070                          //
+      ////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////////// 
+      $navgationBar='<ul>';
+      for ($i=0; $i < $tabCount ; $i++) {
+        
+        $navgationBar .= "<li><a href='".$tabArray["link"][$i]."' >".$tabArray["name"][$i]."</a></li>";
+        
+      }
+      $navgationBar .="</ul>";
+
+      return $navgationBar;
+      
+    }
+    
+    public function html_menu_tab($tabArray)
+    {
+      ////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////
+      ////////////////////                    ////////////////
+      /////////////////   function description  //////////////
+      /////////////////                         //////////////
+      // this function genrate html based on the parameters //
+      // which you put the count of tabs and the array which//
+      // has the data itself and return a string which      //
+      // contain the html <a> menu tab                      //
+      ////////////////////////////////////////////////////////
+      // Developed by engineer Muhammad Abdulla El Nahtta   //
+      // Cell Phone +20 1093001070                          //
+      ////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////////// 
+      $navgationBar="<a class='has-arrow' href='#' aria-expanded='true'>";        
+      $navgationBar .= "<span class='hide-menu'><i class='mdi mdi-audiobook'></i>".$tabArray["name"][0]."</span>";
+      $navgationBar .="</a>";
+  
+      return $navgationBar;
+      
+    }
+
+
+
 }
 
 
@@ -156,111 +306,20 @@ li a:hover:not(.active) {
 
 <?php 
 
-function get_menu($gt_id_parent="", $get_type="", $get_HasChild="", $get_link="")
-{
-
-    // new object for Arabicss db 
-    $db = new RoleBasedDB;
-    //php Creating a dynamic search query with PHP and MySQL
-    $whereArr = array();
-    if ($gt_id_parent != "")
-    {
-    $whereArr[] = "`id_parent` = '{$gt_id_parent}'";
-    }
-    if ($get_type != "")
-    {
-    $whereArr[] = "`type` = '{$get_type}'";
-    }
-    if ($get_HasChild != "") {
-      $whereArr[] = "`HasChild` = '{$get_HasChild}'";
-    } 
-
-    if ($get_link != "") {
-      $whereArr[] = "`link` IS NOT NULL";
-    } 
+$nav = new NavBarMenu;
 
 
-    // escaping the WHERE clause in case no parameter entered 
-    // Array to string for WHERE clause  
-    if ($whereArr == null) {
-      $whereStr = "";
-    } else {
-      $whereStr = "WHERE " . implode(" AND ", $whereArr);
-    }
+//the below function will return an array for both the count and he result 
+$menu= $nav->get_menu("Company","","", "", "");
+$query_itself =$menu['sql_query'];
+$queryResult =$menu['queryResult'];
+$tabCount =$menu['resCount'];
+$tabArray =$menu['navbar'];
 
-    if ($get_HasChild != "") {
-      // getting how many child per id if the value passed
-      $menu_sql_query ="SELECT * FROM (SELECT m1.*, 
-      (SELECT COUNT(*) FROM `module_menue` `m2` WHERE `m2`.`id_Parent`=`m1`.`id`)
-      AS `HasChild` FROM `module_menue` `m1`) AS `testy` {$whereStr} ORDER BY `order_no`;";
-
-    } else {
-      // getting only the main 
-      $menu_sql_query ="SELECT * FROM `module_menue` {$whereStr} ORDER BY `order_no`;";
-
-    }
-
-    // genrate mysql query
-    $queryResult = $db->query($menu_sql_query);
-
-    while($row = $queryResult->fetch_assoc()) {
-      $id[] = $row["id"];
-      $id_parent[] = $row["id_parent"];
-      $icon[] = $row["icon"];
-      $link[] = $row["link"];
-      $name[] = $row["name"];
-      $type[] = $row["type"];
-      $order_no [] = $row["order_no"];
-    }
-    
-    $navBar= array("id"=>$id, "id_parent"=>$id_parent, "icon"=>$icon,
-       "link"=>$link, "name"=>$name, "type"=>$type, "order_no"=>$order_no);
-      
-      // rest variables to avoid confilct 
-      $id[] = '';
-      $id_parent 	[] = '';
-      $icon[] = '';
-      $link[] = '';
-      $name[] = '';
-      $type[] = '';
-      $order_no [] ='';
+$updated_menu=$nav->html_menu_tab($tabArray);
 
 
-    $resCount = $queryResult->num_rows;
-
-    $query_data = array("queryResult"=>$queryResult, "resCount"=>$resCount, "navbar"=>$navBar);
-    return $query_data;
-
-
-}
-
-
-
-function sort_menu($tabCount, $tabArray)
-{
-  $navgationBar='<ul>';
-  for ($i=0; $i < $tabCount ; $i++) {
-     
-    $navgationBar .= "<li><a href='".$tabArray["link"][$i]."' >".$tabArray["name"][$i]."</a></li>";
-    
-  }
-  $navgationBar .="</ul>";
-
-  return $navgationBar;
-  
-}
-
-// the below function will return an array for both the count and he result 
-$res= get_menu("profile","", "", "true");
-$tabCount =$res['resCount'];
-$tabArray =$res['navbar'];
-
-
-
-
-
-
-echo $nvagationbar; 
+echo $updated_menu; 
 
 
 
