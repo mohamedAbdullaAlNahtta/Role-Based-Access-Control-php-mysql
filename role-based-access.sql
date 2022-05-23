3f970efeb4c4f595c16e8088b0cc1b5abc62e42a
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2022-05-23 17:40:03 +0200

    Update role-based-access.sql

--- role-based-access.sql
+++ role-based-access.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2022 at 03:33 PM
+-- Generation Time: May 23, 2022 at 05:39 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
