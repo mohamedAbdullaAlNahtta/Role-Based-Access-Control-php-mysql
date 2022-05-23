792c2c94fc8ef3af14d048a8eb4cd3e9ebd530ea
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2022-05-23 17:57:53 +0200

    Update role-based-access.sql

--- role-based-access.sql
+++ role-based-access.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2022 at 05:39 PM
+-- Generation Time: May 23, 2022 at 05:57 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -79,19 +79,20 @@ INSERT INTO `module_menu` (`id`, `id_parent`, `icon`, `link`, `name`, `type`, `o
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
+  `password` varchar(250) NOT NULL,
+  `user_role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'Ahmed', 'password'),
-(2, 'Mohamed', 'password'),
-(3, 'Ali', 'password'),
-(4, 'Omar', 'password'),
-(5, 'Mahmoud', 'password');
+INSERT INTO `users` (`id`, `username`, `password`, `user_role_id`) VALUES
+(1, 'Ahmed', 'password', 1),
+(2, 'Mohamed', 'password', 1),
+(3, 'Ali', 'password', 2),
+(4, 'Omar', 'password', 2),
+(5, 'Mahmoud', 'password', 3);
 
 -- --------------------------------------------------------
 
@@ -129,6 +130,71 @@ CREATE TABLE `user_role_module_menu` (
   `module_menu_id` varchar(250) NOT NULL,
   `access_type` enum('read','write') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role_module_menu`
+--
+
+INSERT INTO `user_role_module_menu` (`id`, `user_role_id`, `module_menu_id`, `access_type`) VALUES
+(1, 1, 'Home', 'read'),
+(2, 1, 'Company Home', 'read'),
+(3, 1, 'Contact Home', 'read'),
+(4, 1, 'Profile  Home', 'read'),
+(5, 1, 'institution Home', 'read'),
+(6, 1, 'institution user Home', 'read'),
+(7, 1, 'News', 'read'),
+(8, 1, 'Company News', 'read'),
+(9, 1, 'Contact News', 'read'),
+(10, 1, 'Profile  News', 'read'),
+(11, 1, 'institution News', 'read'),
+(12, 1, 'institution user News', 'read'),
+(13, 1, 'Contact', 'read'),
+(14, 1, 'Company Contact', 'read'),
+(15, 1, 'Profile  Contact', 'read'),
+(16, 1, 'institution Contact', 'read'),
+(17, 1, 'institution user Contact', 'read'),
+(18, 1, 'About', 'read'),
+(19, 1, 'Company About', 'read'),
+(20, 1, 'Profile  About', 'read'),
+(21, 1, 'institution About', 'read'),
+(22, 1, 'institution user About', 'read'),
+(23, 1, 'Profile', 'read'),
+(24, 1, 'institution user', 'read'),
+(25, 1, 'Company', 'read'),
+(26, 2, 'institution', 'read'),
+(27, 2, 'institution user Home', 'read'),
+(28, 2, 'Company News', 'read'),
+(29, 2, 'Contact News', 'read'),
+(30, 2, 'Profile  News', 'read'),
+(31, 2, 'institution News', 'read'),
+(32, 2, 'Contact', 'read'),
+(33, 2, 'Company Contact', 'read'),
+(34, 2, 'Profile  Contact', 'read'),
+(35, 2, 'institution Contact', 'read'),
+(36, 2, 'institution user Contact', 'read'),
+(37, 2, 'About', 'read'),
+(38, 2, 'Company About', 'read'),
+(39, 2, 'Profile  About', 'read'),
+(40, 2, 'institution About', 'read'),
+(41, 2, 'institution user About', 'read'),
+(42, 2, 'Profile', 'read'),
+(43, 2, 'institution user', 'read'),
+(44, 2, 'Company', 'read'),
+(45, 2, 'institution', 'read'),
+(46, 3, 'Company Home', 'read'),
+(47, 3, 'Contact Home', 'read'),
+(48, 3, 'Profile  Home', 'read'),
+(49, 3, 'institution Home', 'read'),
+(50, 3, 'institution user Home', 'read'),
+(51, 3, 'Contact', 'read'),
+(52, 3, 'Company Contact', 'read'),
+(53, 3, 'Profile  Contact', 'read'),
+(54, 3, 'institution Contact', 'read'),
+(55, 3, 'institution user Contact', 'read'),
+(56, 3, 'Profile', 'read'),
+(57, 3, 'institution user', 'read'),
+(58, 3, 'Company', 'read'),
+(59, 3, 'institution', 'read');
 
 --
 -- Indexes for dumped tables
@@ -179,7 +245,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user_role_module_menu`
 --
 ALTER TABLE `user_role_module_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
